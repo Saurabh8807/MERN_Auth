76dbb4e860d469fa885ce60e8bdb6cd3d8be1387
Saurabh8807 <sjagtap18092002@gmail.com>
2024-07-22 23:18:21 +0530

    usernname issue !!!!

--- client/src/pages/signUp.jsx
+++ client/src/pages/signUp.jsx
@@ -1,10 +1,73 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function signUp() {
+function SignUp() {
+  const [formData, setFormSData] = useState({})
+
+  
+  
+  const handleChange = (e) => {
+    setFormSData({...formData,[e.target.id]:e.target.value})
+  };
+
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    const res =  await fetch("/api/auth/signup",{
+      method:"POST",
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(formData)
+    })
+    const data = await res.json()
+    console.log(data)
+    
+  }
+  // console.log(formData)
   return (
-    <div>signUp</div>
-  )
+    <>
+      <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            className="bg-slate-100 p-3 rounded-lg"
+            onChange={handleChange}
+          />
+          <button
+            className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            Sign Up
+          </button>
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Have an account?</p>
+          <Link to="/sigin">
+            <span className="text-blue-500">Sign in</span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default signUp
+export default SignUp;
